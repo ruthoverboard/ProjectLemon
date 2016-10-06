3e4b2a4c9926048c1cccf353c79c097901f9d9c2
HernanMedH <alexyz656@gmail.com>
2016-10-06 01:35:55 -0700

    CurrentLocation y Permissions

--- app/src/main/java/com/example/projectlemon/projectlemon/MapsActivity.java
+++ app/src/main/java/com/example/projectlemon/projectlemon/MapsActivity.java
@@ -1,13 +1,20 @@
 package com.example.projectlemon.projectlemon;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.Manifest;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -19,9 +26,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.security.Provider;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
+
 
     private GoogleMap mMap;
 
@@ -33,18 +43,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-90, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        //LocationManager locationManager = Context.getSystemService(Context.LOCATION_SERVICE);
+        int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+        boolean per = false;
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+                ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+
+        }else{
+            per = true;
+        }
+        // Permission was granted.
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            mMap.setMyLocationEnabled(true);
+        }else {
+
+                    // Permission denied, Disable the functionality that depends on this permission.
+            Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+        }
+
     }
+
+
+       /* LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String s = locationManager.getBestProvider(criteria, false);
+        try{
+            Location location = locationManager.getLastKnownLocation(s);
+            LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(currentLatLng).title("You are here"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+        }catch (SecurityException ex){
+
+
+        }*/
+
+        //LocationManager locationManager = Context.getSystemService(Context.LOCATION_SERVICE);
+
 
     @Override
     public void onLocationChanged(Location location) {
