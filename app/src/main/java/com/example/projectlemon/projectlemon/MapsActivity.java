70141dd95f4e7100b146306b92d3b6819189e3dc
HernanMedH <alexyz656@gmail.com>
2016-10-07 00:10:16 -0700

    Llamada al API de Directions sin dibujo de PolyLines

--- app/src/main/java/com/example/projectlemon/projectlemon/MapsActivity.java
+++ app/src/main/java/com/example/projectlemon/projectlemon/MapsActivity.java
@@ -1,11 +1,13 @@
 package com.example.projectlemon.projectlemon;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -14,6 +16,8 @@ import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.Manifest;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -26,6 +30,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.Provider;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
@@ -34,6 +47,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     private GoogleMap mMap;
+    private LatLng myLatLng;
+    LatLng latLngCetys = new LatLng(32.50660123141241, -116.92439664155245);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,59 +58,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        final Button button = (Button) findViewById(R.id.btnRoute);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getRoute();
+            }
+        });
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-        boolean per = false;
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED)
-        {
-                ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_LOCATION);
-
-        }else{
-            per = true;
-        }
-        // Permission was granted.
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-
-            mMap.setMyLocationEnabled(true);
-        }else {
-
-                    // Permission denied, Disable the functionality that depends on this permission.
-            Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+        getPermissions();
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            Location location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+             myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, 17));
+        }catch (SecurityException ex){
+            Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
         }
 
     }
 
 
-       /* LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String s = locationManager.getBestProvider(criteria, false);
-        try{
-            Location location = locationManager.getLastKnownLocation(s);
-            LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(currentLatLng).title("You are here"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
-        }catch (SecurityException ex){
-
-
-        }*/
-
-        //LocationManager locationManager = Context.getSystemService(Context.LOCATION_SERVICE);
-
 
     @Override
     public void onLocationChanged(Location location) {
+        //LatLng mylatlng = new LatLng(location.getLatitude(), location.getLongitude());
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mylatlng, 17));
 
     }
 
@@ -128,4 +121,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    public void getPermissions(){
+
+        int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+        boolean per = false;
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+
+        }else{
+            per = true;
+        }
+        // Permission was granted.
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            mMap.setMyLocationEnabled(true);
+        }else {
+
+            // Permission denied, Disable the functionality that depends on this permission.
+            Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void getRoute(){
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin="
+                + myLatLng.latitude + "," + myLatLng.longitude +
+                "&destination=" + latLngCetys.latitude + "," + latLngCetys.longitude +
+                "&key=AIzaSyCq7XqwYUeGOVLqs4FzvjDrYYRGLEar3-A";
+        RetrieveFeedTask r = new RetrieveFeedTask();
+        r.myLatLng = myLatLng;
+        r.latLngCetys = latLngCetys;
+        r.execute();
+        Toast.makeText(this, "It WORKS!", Toast.LENGTH_LONG).show();
+    }
 }
+class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+    private Exception exception;
+    public LatLng myLatLng;
+    public LatLng latLngCetys;
+
+    protected void onPreExecute() {
+
+    }
+
+    protected String doInBackground(Void... urls) {
+       //String email = emailText.getText().toString();
+        // Do some validation here
+
+        try {
+            URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin="
+                    + myLatLng.latitude + "," + myLatLng.longitude +
+                    "&destination=" + latLngCetys.latitude + "," + latLngCetys.longitude +
+                    "&key=AIzaSyCq7XqwYUeGOVLqs4FzvjDrYYRGLEar3-A");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }else {
+
+            String s = response;
+        }
+
+        // TODO: check this.exception
+        // TODO: do something with the feed
+        /*
+            try {
+                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                String s = (String) object.get("status");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }*/
+    }
+}
+
+
