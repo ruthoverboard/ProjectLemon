73852805685e35e009afa35a4dfce9053d442853
HernanMedH <alexyz656@gmail.com>
2016-10-18 02:22:12 -0700

    Agregar Kinesis stream con location change

--- app/src/main/java/com/example/projectlemon/projectlemon/MapsActivity.java
+++ app/src/main/java/com/example/projectlemon/projectlemon/MapsActivity.java
@@ -8,6 +8,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -38,10 +40,17 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import com.amazonaws.mobileconnectors.kinesis.kinesisrecorder.*;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
 import java.security.Provider;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
@@ -51,6 +60,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private LatLng myLatLng;
     LatLng latLngCetys = new LatLng(32.50660123141241, -116.92439664155245);
+    public static KinesisRecorder recorder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +70,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final Bundle pass = getIntent().getExtras();
 
         setContentView(R.layout.activity_maps);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -84,6 +99,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        recorder = setUpAWS();
+
+
+
     }
 
     @Override
@@ -99,15 +118,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }catch (SecurityException ex){
             Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
         }
-
     }
 
 
 
     @Override
     public void onLocationChanged(Location location) {
-        //LatLng mylatlng = new LatLng(location.getLatitude(), location.getLongitude());
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mylatlng, 17));
+
+            recorder.saveRecord(location.toString().getBytes(), "ProjectLemonStream");
+            //Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
+
+        try{
+            recorder.submitAllRecords();
+        }catch (Exception ex){
+            Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
+
+        }
 
     }
 
@@ -180,6 +206,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         r.execute();
         Toast.makeText(this, "It WORKS!", Toast.LENGTH_LONG).show();
     }
+
+
+    public KinesisRecorder setUpAWS(){
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-east-1:a0c3cc8d-5bb5-4c28-b4b7-9282805a37d3", // Identity Pool ID
+                Regions.US_EAST_1 // Region
+        );
+        String path = "CetysCarpool";
+        KinesisRecorder rec;
+        try{
+        rec = new KinesisRecorder(
+                this.getDir(path, 0), // An empty directory KinesisRecorder can use for storing requests
+                Regions.US_EAST_1,  // Region that this Recorder should save and send requests to
+                credentialsProvider);
+            return rec;
+
+        }catch (Exception ex){
+            Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
+            return null;
+        }
+
+    }
+
 }
 class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
     private Exception exception;
@@ -239,6 +289,9 @@ class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
                 e.printStackTrace();
             }*/
     }
+
+
+
 }
 
 
