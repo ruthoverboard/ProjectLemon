f68ddd8646df95781bd63c789eda3ead7ac6183d
HernanMedH <alexyz656@gmail.com>
2016-10-18 02:23:10 -0700

    Agregar AWS sdk jars

---
+++ app/src/main/java/com/example/projectlemon/projectlemon/AWSHelper.java
@@ -0,0 +1,13 @@
+package com.example.projectlemon.projectlemon;
+
+import android.os.Environment;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.kinesis.kinesisrecorder.KinesisRecorder;
+import com.amazonaws.regions.Regions;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+public class AWSHelper {
+
+}
