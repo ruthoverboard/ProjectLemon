2b8673b4ae6fa6f77c58f8c4948b315c92b66d43
Ruth Celiceo <ruthceliceo94@gmail.com>
2016-11-21 09:48:11 -0800

    Phone number activity

---
+++ app/src/main/java/com/example/projectlemon/projectlemon/askPhoneNumber.java
@@ -0,0 +1,13 @@
+package com.example.projectlemon.projectlemon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class askPhoneNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_phone_number);
+    }
+}
