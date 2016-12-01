b7d854c3d1c7f79224af7badf55e647360ae8a86
HernanMedH <alexyz656@gmail.com>
2016-12-01 00:59:32 -0800

    Fixing this I messed up

--- app/src/main/java/com/example/projectlemon/projectlemon/MainActivity.java
+++ app/src/main/java/com/example/projectlemon/projectlemon/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.projectlemon.projectlemon;
 
 import android.content.Intent;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,10 +14,25 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,16 +46,20 @@ public class MainActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
 
         setContentView(R.layout.activity_main);
-
-
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email", "public_profile", "user_friends");
 
+        if(isLoggedIn())
+        {
+            startActivity(new Intent(MainActivity.this, UserProfileActivity.class));
+        }
+
+
         final Button button = (Button) findViewById(R.id.btnMaps);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                startActivity(new Intent(MainActivity.this, MapsActivityPedirRaite.class));
             }
         });
 
@@ -47,8 +68,82 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         //query que manda a buscar al usuario
-                        AccessToken accessToken = loginResult.getAccessToken();
-                        Profile profile = Profile.getCurrentProfile();
+
+                        final AccessToken accessToken = AccessToken.getCurrentAccessToken();
+                        Log.d("e", accessToken.toString());
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                accessToken,
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(
+                                            JSONObject object,
+                                            GraphResponse response) {
+                                        // Application code
+                                        String idUser = null;
+                                        try {
+                                            idUser = object.getString("id");
+                                            Log.d("request", idUser);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                        Log.d("json:", object.toString());
+
+                                        String url = "https://p4x0vleufi.execute-api.us-east-1.amazonaws.com/dev/searchUser/" + idUser;
+                                        Log.d("url", url);
+                                        AsyncHttpClient client = new AsyncHttpClient();
+                                        final String finalIdUser = idUser;
+                                        client.get(url, new JsonHttpResponseHandler() {
+                                            @Override
+                                            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                                                //super.onFailure(statusCode, headers, throwable, errorResponse);
+                                                Log.d("nope", String.valueOf(errorResponse));
+
+                                            }
+                                            @Override
+                                            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                                                //super.onSuccess(statusCode, headers, response);
+                                                Log.d("yay2", response.toString());
+                                                try {
+                                                    String idUserDB = response.getJSONObject(0).get("idUser").toString();
+                                                    Log.d("yay2", idUserDB);
+                                                    Log.d("yay22", finalIdUser);
+                                                    if(finalIdUser.equals(idUserDB)) {
+                                                        Log.d("wtf", finalIdUser);
+                                                        startActivity(new Intent(MainActivity.this, UserProfileActivity.class));
+                                                    }
+                                                    else{
+                                                        startActivity(new Intent(MainActivity.this, firstLogin.class));
+                                                    }
+                                                } catch (JSONException e) {
+                                                    startActivity(new Intent(MainActivity.this, firstLogin.class));
+                                                    e.printStackTrace();
+                                                }
+
+
+                                            }
+                                            @Override
+                                            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                                                super.onFailure(statusCode, headers, throwable, errorResponse);
+                                                Log.d("nope2", String.valueOf(errorResponse));
+
+
+                                            }
+                                            @Override
+                                            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                                                super.onFailure(statusCode, headers, responseString, throwable);
+                                                Log.d("nope3", String.valueOf(responseString));
+                                                startActivity(new Intent(MainActivity.this, firstLogin.class));
+                                            }
+                                        });
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+                        //AccessToken accessToken = loginResult.getAccessToken();
+                        //Profile profile = Profile.getCurrentProfile();
 
                         //Bundle pass = new Bundle();
                         //Intent intent = new Intent(getApplicationContext(), UserProfileActivity.class);
@@ -57,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
                         //intent.putExtras(pass);
                         //intent.putExtra("profile", profile.getName());
                         //startActivity(intent);
-                        startActivity(new Intent(MainActivity.this, firstLogin.class));
+
                     }
 
                     @Override
@@ -76,5 +171,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
     }
 }
--- app/src/main/java/com/example/projectlemon/projectlemon/askPhoneNumber.java
+++ app/src/main/java/com/example/projectlemon/projectlemon/askPhoneNumber.java
@@ -1,12 +1,33 @@
 package com.example.projectlemon.projectlemon;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.loopj.android.http.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class askPhoneNumber extends AppCompatActivity {
     String phoneNumber;
@@ -22,24 +43,117 @@ public class askPhoneNumber extends AppCompatActivity {
         if(extras != null){
             career = extras.getString("career");
         }
-        EditText phone = (EditText)findViewById(R.id.txtPhone);
-        phoneNumber = phone.getText().toString();
+
+
+
 
         Button btnProfile = (Button) findViewById(R.id.btnContinuar);
         btnProfile.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(askPhoneNumber.this, UserProfileActivity.class);
-                Bundle bndl = new Bundle();
+                final Intent intent = new Intent(askPhoneNumber.this, UserProfileActivity.class);
+                //Bundle bndl = new Bundle();
+
+                EditText phone = (EditText)findViewById(R.id.txtPhone);
+                phoneNumber = phone.getText().toString();
 
                 if(phoneNumber != null){
-                    bndl.putString("phoneNumber", phoneNumber);
-                    bndl.putString("career", career);
-                    intent.putExtras(bndl);
+                    //bndl.putString("phoneNumber", phoneNumber);
+                    //bndl.putString("career", career);
+                    //intent.putExtras(bndl);
+
+
+
+                    final String url = "https://p4x0vleufi.execute-api.us-east-1.amazonaws.com/dev/createUser";
+
+                    AccessToken accessToken = AccessToken.getCurrentAccessToken();
+
+                    GraphRequest request = GraphRequest.newMeRequest(
+                            accessToken,
+                            new GraphRequest.GraphJSONObjectCallback() {
+                                @Override
+                                public void onCompleted(
+                                        JSONObject object,
+                                        GraphResponse response) {
+                                    // Application code
+                                    try {
+                                        //JSONObject obj = new JSONObject();
+                                        /*try {
+                                            obj.put("id", object.getString("id"));
+                                            obj.put("name", object.getString("name"));
+                                            obj.put("phoneNumber", phoneNumber);
+                                            obj.put("career", career);
+                                            obj.put("email", object.getString("email"));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }*/
+                                        StringEntity params = null;
+                                        try {
+                                            params = new StringEntity("{ "
+                                                    + "\"id\": " + object.getString("id")
+                                                    + ", \"name\": " + "\"" + object.getString("name") + "\""
+                                                    + ", \"phoneNumber\": " + "\"" + phoneNumber + "\""
+                                                    + ", \"career\": " + "\"" + career + "\""
+                                                    + ", \"email\": " + "\"" + object.getString("email") + "\""
+                                                    +  " }");
+
+                                        } catch (UnsupportedEncodingException e) {
+                                            e.printStackTrace();
+                                        }
+                                        Log.d("num", phoneNumber);
+
+                                        AsyncHttpClient client = new AsyncHttpClient();
+                                        client.post(getApplicationContext(), url, params, "application/json", new AsyncHttpResponseHandler() {
+                                            @Override
+                                            public void onStart() {
+                                                // Initiated the request
+                                            }
+
+                                            @Override
+                                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                                // Successfully got a response
+                                                Log.d("yay", String.valueOf(responseBody));
+                                                startActivity(intent);
+                                            }
+
+                                            @Override
+                                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                                                Log.d("nope", String.valueOf(error));
+                                            }
+
+                                        });
+
+
+                                    } catch (JSONException e) {
+
+                                    }
+                                    Log.d("json:", object.toString());
+                                }
+                            });
+
+                    Bundle parameters = new Bundle();
+                    parameters.putString("fields", "id, email, name");
+                    request.setParameters(parameters);
+                    request.executeAsync();
+
+
+                    //JSONObject params = new JSONObject();
+
+                    //int a = 123456;
+                    //params.put("id", 123456);
+                    //params.put("name", "asd");
+                    //params.put("email", "asd");
+                    //params.put("phoneNumber", "6642214815");
+                    //RequestParams params = new RequestParams("par", obj.toString());
+
+
                 }
-                startActivity(intent);
+                //startActivity(intent);
             }
         });
     }
+
+
 }
