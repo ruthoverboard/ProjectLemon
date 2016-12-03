56bcf9c3a8f55d9da2c3a964b06a4fc38b9ec195
toco94 <amejia@cetys.edu.mx>
2016-12-03 00:52:03 -0800

    Get Requests Done, including POST

--- app/src/main/java/com/example/projectlemon/projectlemon/askPhoneNumber.java
+++ app/src/main/java/com/example/projectlemon/projectlemon/askPhoneNumber.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.kevinsawicki.http.HttpRequest;
+import com.goebl.david.Webb;
 import com.loopj.android.http.*;
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
@@ -27,7 +28,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Map;
 
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
@@ -88,19 +98,15 @@ public class askPhoneNumber extends AppCompatActivity {
                                         } catch (JSONException e) {
                                             e.printStackTrace();
                                         }*/
-                                        StringEntity params = null;
-                                        try {
-                                            params = new StringEntity("{ "
-                                                    + "\"id\": " + object.getString("id")
-                                                    + ", \"name\": " + "\"" + object.getString("name") + "\""
-                                                    + ", \"phoneNumber\": " + "\"" + phoneNumber + "\""
-                                                    + ", \"career\": " + "\"" + career + "\""
-                                                    + ", \"email\": " + "\"" + object.getString("email") + "\""
-                                                    +  " }");
+                                        //String params = null;
+                                        String params = "{ "
+                                                + "\"id\": " + object.getString("id")
+                                                + ", \"name\": " + "\"" + object.getString("name") + "\""
+                                                + ", \"phoneNumber\": " + "\"" + phoneNumber + "\""
+                                                + ", \"career\": " + "\"" + career + "\""
+                                                + ", \"email\": " + "\"" + object.getString("email") + "\""
+                                                +  " }";
 
-                                        } catch (UnsupportedEncodingException e) {
-                                            e.printStackTrace();
-                                        }
                                         Log.d("num", phoneNumber);
 
                                         new GetHttpRequest().execute(params);
@@ -165,23 +171,92 @@ public class askPhoneNumber extends AppCompatActivity {
 
 
 
-    private class GetHttpRequest extends AsyncTask<StringEntity, Object, Boolean> {
+    private class GetHttpRequest extends AsyncTask<String, Object, Boolean> {
         @Override
-        protected Boolean doInBackground(StringEntity... params) {
-            //JSONArray response = null;
+        protected Boolean doInBackground(String... params) {
             Boolean bool = null;
-
+            JSONObject obj = null;
             String url = "https://p4x0vleufi.execute-api.us-east-1.amazonaws.com/dev/createUser";
-            HttpRequest.post(url).send(params[0].toString()).code();
+            try {
+                obj = new JSONObject(params[0]);
 
+/*
+                //for testing
+                JSONObject jo = new JSONObject();
+                jo.put("id", 120678);
+                jo.put("name", "Doe");
+                jo.put("phoneNumber", "John");
+                jo.put("career", "Doesss");
+                jo.put("email", "John");
+*/
+                Log.d("My App", obj.toString());
+                Log.d("phonetype value ", obj.getString("career"));
+
+                StringBuilder sb = new StringBuilder();
+                HttpURLConnection urlConnection=null;
+                URL url2 = new URL(url);
+                urlConnection = (HttpURLConnection) url2.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setUseCaches(false);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setRequestProperty("Content-Type","application/json");
+                urlConnection.connect();
+
+                OutputStreamWriter out = new OutputStreamWriter(urlConnection.getOutputStream());
+                out.write(obj.toString());
+                out.close();
+
+                int HttpResult =urlConnection.getResponseCode();
+                if(HttpResult ==HttpURLConnection.HTTP_OK){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(
+                            urlConnection.getInputStream(),"utf-8"));
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    br.close();
+
+                    Log.d("woah", ""+sb.toString());
+                }else{
+                    Log.d("woah", urlConnection.getResponseMessage());
+
+                }
+
+
+                /*
+                Webb webb = Webb.create();
+                webb.setDefaultHeader("Connection", "close");
+                JSONObject result = webb.post(url)
+                        .body(jo)
+                        .ensureSuccess()
+                        .asJsonObject()
+                        .getBody();
+
+                Log.d("WORKS", result.toString());
+                */
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.d("wtf", params[0].toString());
+            bool = true;
+            return bool;
+
+            //HttpRequest.post(url).send(params[0].toString()).code();
             //response = new JSONArray(HttpRequest.get(url).body());
             //String idUserDB = response.getJSONObject(0).get("idUser").toString();
             //Log.d("HttpSNAP", idUserDB);
-
             //if (params[0].equals(idUserDB)) {
-            Log.d("wtf", params[0].toString());
+
             //startActivity(new Intent(MainActivity.this, UserProfileActivity.class));
-            bool = true;
+
             //} else {
             //startActivity(new Intent(MainActivity.this, firstLogin.class));
             //    bool = false;
@@ -189,14 +264,13 @@ public class askPhoneNumber extends AppCompatActivity {
 
 
             //Log.d("HttpReq", response.toString());
-            return bool;
+
             //return response;
 
         }
 
         @Override
         protected void onPostExecute(Boolean result) {
-            Log.d("HttpReq", result.toString());
             if (result == true){
                 startActivity(new Intent(askPhoneNumber.this, UserProfileActivity.class));
             }
